309be48edb41537408436e157cbf00e38753e9f4
karman-ly <karmanlysenko@gmail.com>
2022-10-02 15:02:41 +0200

    refactored project structure and added submit function placeholder

--- src/index.js
+++ src/index.js
@@ -3,169 +3,194 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 
 function TextInput({name, placeholder, onChange, onKeyDown, formData}) {
-  return (
-      <input
-          required
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-          placeholder={placeholder}
-          name={name}
-          value={formData[name]}
-      />
-  )
+    return (
+        <input
+            required
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            placeholder={placeholder}
+            name={name}
+            value={formData[name]}
+        />
+    )
+}
+
+function TextInputSet({onChange, onKeyDown, formData, textInputs}) {
+
+    const textElements = textInputs.map(input => (
+        <TextInput
+            key={input.name}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            formData={formData}
+            {...input}
+        />
+    ))
+
+    return (
+        {textElements: textElements}
+    )
 }
 
 function RadioInput({label, value, onChange, onKeyDown, formData}) {
-  return (
-      <div className="radio-container">
-        <input
-            required
-            type="radio"
+    return (
+        <div className="radio-container">
+            <input
+                required
+                type="radio"
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                checked={formData.employment === value}
+                name="employment"
+                value={value}
+            />
+            <label htmlFor={value}>{label}</label>
+        </div>
+    )
+}
+
+function RadioInputSet({legend, onChange, onKeyDown, formData, radios}) {
+
+    const radioElements = radios.map(radio => (
+        <RadioInput
+            key={radio.value}
             onChange={onChange}
             onKeyDown={onKeyDown}
-            checked={formData.employment === value}
-            name="employment"
-            value={value}
+            formData={formData}
+            {...radio}
         />
-        <label htmlFor={value}>{label}</label>
-      </div>
-  )
+    ))
+
+    return (
+        <fieldset>
+            <legend>{legend}</legend>
+            {radioElements}
+        </fieldset>
+    )
 }
 
 function App() {
 
-  const [formData, setFormData] = React.useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comments: "",
-    isFriendly: false,
-    employment: "",
-    favouriteColor: ""
-  })
-
-  console.log(formData)
-
-  function handleChange(event) {
-    const {name, value, type, checked} = event.target
-    setFormData(formData => {
-      return {
-        ...formData,
-        [name]: type === "checkbox" ? checked : value
-      }
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        comments: "",
+        isFriendly: false,
+        employment: "",
+        favouriteColor: ""
     })
-  }
 
-  function handleKeyDown(event) {
-    const {key, target} = event
-    const form = target.form
-    if (key === "ArrowDown" || key === "ArrowUp") {
-      event.preventDefault()
-      const index = Array.from(form).indexOf(target)
-
-      let nextItem = form[index + 1]
-      let prevItem = form[index - 1]
-
-      if (nextItem?.tagName === "FIELDSET")
-        nextItem = form[index + 2]
-      if (prevItem?.tagName === "FIELDSET")
-        prevItem = form[index - 2]
-
-      if (key === "ArrowDown")
-        nextItem?.focus()
-      else if (key === "ArrowUp")
-        prevItem?.focus()
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(formData => ({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value
+        }))
     }
-    else if (key === "Enter" && (target.type === "checkbox" || target.type === "radio")) {
-      event.preventDefault()
-      target.checked = !target.checked
-      handleChange(event)
+
+    function handleKeyDown(event) {
+        const {key, target} = event
+        const form = target.form
+        if (key === "ArrowDown" || key === "ArrowUp") {
+            event.preventDefault()
+            const index = Array.from(form).indexOf(target)
+
+            let nextItem = form[index + 1]
+            let prevItem = form[index - 1]
+
+            if (nextItem?.tagName === "FIELDSET")
+                nextItem = form[index + 2]
+            if (prevItem?.tagName === "FIELDSET")
+                prevItem = form[index - 2]
+
+            if (key === "ArrowDown")
+                nextItem?.focus()
+            else if (key === "ArrowUp")
+                prevItem?.focus()
+        }
+        else if (key === "Enter" && (target.type === "checkbox" || target.type === "radio")) {
+            event.preventDefault()
+            target.checked = !target.checked
+            handleChange(event)
+        }
+        else if (key === "Enter" && target.type !== "submit" && target.tagName !== "SELECT") {
+            event.preventDefault()
+        }
     }
-    else if (key === "Enter" && target.type !== "submit" && target.tagName !== "SELECT") {
-       event.preventDefault()
+
+    function submit(event) {
+        event.preventDefault()
+        console.log(formData)
     }
-  }
 
-  const textElements = [
-      {placeholder: "First name", name: "firstName"},
-      {placeholder: "Last name", name: "lastName"},
-      {placeholder: "Email", name: "email"}
-  ].map(input => (
-      <TextInput
-          key={input.name}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          formData={formData}
-          {...input}
-      />
-  ))
+    return (
+        <div className="app">
+            <form className="form" onSubmit={submit}>
+                <TextInputSet
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    formData={formData}
+                    textInputs={[
+                        {placeholder: "First name", name: "firstName"},
+                        {placeholder: "Last name", name: "lastName"},
+                        {placeholder: "Email", name: "email"}
+                    ]}
+                />
 
-  const radioElements = [
-    {value: "unemployed", label: "Unemployed"},
-    {value: "part-time", label: "Part-time"},
-    {value: "full-time", label: "Full-time"},
-  ].map(input => (
-      <RadioInput
-          key={input.value}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          formData={formData}
-          {...input}
-      />
-  ))
+                <textarea
+                    placeholder="Any comments?"
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    name="comments"
+                    value={formData.comments}
+                />
 
-  return (
-      <div className="app">
-        <form className="form">
-          {textElements}
+                <div className="checkbox-container">
+                    <input
+                        type="checkbox"
+                        id="isFriendly"
+                        checked={formData.isFriendly}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
+                        name="isFriendly"
+                    />
+                    <label htmlFor="isFriendly">Are you Friendly?</label>
+                </div>
 
-          <textarea
-              placeholder="Any comments?"
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
-              name="comments"
-              value={formData.comments}
-          />
+                <RadioInputSet
+                    legend="Current employment status"
+                    formData={formData}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    radios={[
+                        {value: "unemployed", label: "Unemployed"},
+                        {value: "part-time", label: "Part-time"},
+                        {value: "full-time", label: "Full-time"},
+                    ]}
+                />
 
-          <div className="checkbox-container">
-            <input
-                type="checkbox"
-                id="isFriendly"
-                checked={formData.isFriendly}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                name="isFriendly"
-            />
-            <label htmlFor="isFriendly">Are you Friendly?</label>
-          </div>
+                <select
+                    id="favouriteColor"
+                    required
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    name="favouriteColor"
+                    value={formData.favouriteColor}>
+                    <option value="" className="default-option">-- Your favourite color --</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="indigo">Indigo</option>
+                    <option value="violet">Violet</option>
+                </select>
 
-          <fieldset>
-            <legend>Current employment status</legend>
-            {radioElements}
-          </fieldset>
-
-          <select
-              id="favouriteColor"
-              required
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
-              name="favouriteColor"
-              value={formData.favouriteColor}
-          >
-            <option value="" className="default-option">-- Your favourite color --</option>
-            <option value="red">Red</option>
-            <option value="orange">Orange</option>
-            <option value="yellow">Yellow</option>
-            <option value="blue">Blue</option>
-            <option value="green">Green</option>
-            <option value="indigo">Indigo</option>
-            <option value="violet">Violet</option>
-          </select>
-
-          <button className="submit-button" onKeyDown={handleKeyDown}>Submit</button>
-        </form>
-      </div>
-  )
+                <button className="submit-button" onKeyDown={handleKeyDown}>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
